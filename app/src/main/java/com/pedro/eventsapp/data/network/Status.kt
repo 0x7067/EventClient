654233d80186b41e74e9f63c5d5f23ea3ce08a06
Pedro Guimaraes <moisespedro15@gmail.com>
2020-04-18 17:03:05 -0300

    Create event list view model

---
+++ app/src/main/java/com/pedro/eventsapp/data/network/Status.kt
@@ -0,0 +1,6 @@
+package com.pedro.eventsapp.data.network
+
+enum class Status {
+    SUCCESS,
+    ERROR
+}
--- app/src/main/java/com/pedro/eventsapp/ui/features/eventlist/ListEventsViewModel.kt
+++ app/src/main/java/com/pedro/eventsapp/ui/features/eventlist/ListEventsViewModel.kt
@@ -1,2 +1,20 @@
 package com.pedro.eventsapp.ui.features.eventlist
 
+import androidx.lifecycle.ViewModel
+import com.pedro.eventsapp.api.EventsClient
+import com.pedro.eventsapp.data.EventItem
+import com.pedro.eventsapp.data.network.Resource
+import com.pedro.eventsapp.data.network.ResponseHandler
+
+class ListEventsViewModel(): ViewModel() {
+    lateinit var responseHandler: ResponseHandler
+    lateinit var eventsClient: EventsClient
+
+    suspend fun fetchEventsList() : Resource<List<EventItem>> {
+        return try {
+            responseHandler.handleSuccess(eventsClient.getEvents())
+        } catch (e: Exception) {
+            responseHandler.handleException(e)
+        }
+    }
+}
